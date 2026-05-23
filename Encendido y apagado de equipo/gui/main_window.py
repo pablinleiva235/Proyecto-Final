6efd90660bfdb3e3f559618f0eee3c35636d56d7
pablinleiva235 <pabloleiva@frba.utn.edu.ar>
2026-05-23 13:28:44 -0300

    Actualizo agregando maquina de estados

--- Encendido y apagado de equipo/gui/main_window.py
+++ Encendido y apagado de equipo/gui/main_window.py
@@ -1,6 +1,7 @@
 from config.digital_signals import ACTIVE, INACTIVE
 from PyQt5 import QtCore, QtGui, QtWidgets
 from gui.powerOnGUI import Ui_MainWindow
+from services.system_state import SystemState
 
 # Esta es la clase que se llama en Main y dentro de esta se llama a la UI generada en PyQt
 class MainWindow(QtWidgets.QMainWindow):
@@ -9,33 +10,52 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.hw = hardware
 
-        # crear interfaz
+        # Crear interfaz
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # Paso al primer estado: PRE_ENCENDIDO
+        self.change_state(SystemState.PRE_ENCENDIDO)
+
+
+    # =========================================================
+    # FUNCION DE CAMBIO DE ESTADO
+    # =========================================================
+    def change_state(self, new_state):
+        print(f"STATE: {self.current_state} -> {new_state}")
+        self.current_state = new_state
+        if new_state == SystemState.PRE_ENCENDIDO:
+            self.PreEncendido_init()
+        elif new_state == SystemState.MAIN_MENU:
+            self.MainMenu_init()
+
+    # =========================================================
+    # FUNCIONES DEL ESTADO 0: PRE-ENCENDIDO
+    # =========================================================
+
+    def PreEncendido_init(self):
         # Ocultar barra inicialmente
         self.ui.PreEncendido_progressBar.hide()
         # Variables startup
         self.startup_progress = 0
         # Timer startup
         self.startup_timer = QtCore.QTimer()
-        # Cada timeout llama a update_startup()
-        self.startup_timer.timeout.connect(self.update_startup)
-        # Timer para leer POWER_ON_SWITCH
+        self.startup_timer.timeout.connect(self.PreEncendido_update_progressbar)
+        # Timer POWER_ON_SWITCH
         self.power_timer = QtCore.QTimer()
-        self.power_timer.timeout.connect(self.check_power_on)
+        self.power_timer.timeout.connect(self.PreEncendido_check_power_on)
         # Leer entrada cada 100 ms
         self.power_timer.start(100)
 
     # Chequeo de si se pulso ON al inicio cada 100ms
-    def check_power_on(self):
+    def PreEncendido_check_power_on(self):
         if self.hw.digital_read("POWER_ON_SWITCH"):
             self.power_timer.stop()
-            self.startup_sequence()
+            self.PreEncendido_startup_sequence()
 
     # Si se pulso ON, realiza esta secuencia:
     # Habilita señal POWER_ON, arranca timer de 10s, muestra barra de progreso, luego de ese tiempo inicializa placas y pasa a Main Menu
-    def startup_sequence(self):
+    def PreEncendido_startup_sequence(self):
         # Activar retención
         self.hw.digital_set("POWER_ON", ACTIVE)
         # Cambiar texto
@@ -50,18 +70,21 @@ class MainWindow(QtWidgets.QMainWindow):
         # Arrancar timer
         self.startup_timer.start(100)
 
-    def update_startup(self):
+    def PreEncendido_update_progressbar(self):
         self.startup_progress += 1
         self.ui.PreEncendido_progressBar.setValue(self.startup_progress)
         # 100 pasos x 100ms = 10 segundos
         if self.startup_progress >= 100:
             self.startup_timer.stop()
-            # Ir al menú principal
-            self.ui.stackedWidget.setCurrentIndex(1)
-            # Arrancar lógica del menú principal
-            #self.initialize_main_menu()
+            # Paso a estado MAIN_MENU
+            self.change_state(SystemState.MAIN_MENU)
 
-    #def initialize_main_menu(self):
+    # =========================================================
+    # FUNCIONES DEL ESTADO 1: MAIN_MENU
+    # =========================================================
+    def MainMenu_init(self):
+        # Ir al menú principal --> cambio de pagina
+        self.ui.stackedWidget.setCurrentIndex(1)
 
     def closeEvent(self, event):
         self.hw.close()
